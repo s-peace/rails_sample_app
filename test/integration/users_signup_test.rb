1c1dcd93c34aa3f93c281983d179c98b75595f12
s-peace <ssmim777@gmail.com>
2019-08-28 00:22:33 +0900

    amend account activation

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,5 +1,3 @@
-require 'test_helper'
-
 class UsersSignupTest < ActionDispatch::IntegrationTest
 
   def setup
@@ -9,16 +7,16 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   test "invalid signup information" do
     get signup_path
     assert_no_difference 'User.count' do
-      post users_path, params: {user: {name: "",
-                                      email: "user@invalid",
-                                      password: "foo",
-                                      password_confirmation: "bar"}}
-      end
-      assert_template 'users/new'
-      assert_select 'div#error_explanation'
-      assert_select 'div.field_with_errors'
+      post users_path, params: { user: { name:  "",
+                                         email: "user@invalid",
+                                         password:              "foo",
+                                         password_confirmation: "bar" } }
+    end
+    assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.field_with_errors'
   end
-
+    
   test "valid signup information with account activation" do
     get signup_path
     assert_difference 'User.count', 1 do
