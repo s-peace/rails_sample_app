82326bc2e983ef2c58185d2f7d5e9ed9db0209cb
s-peace <ssmim777@gmail.com>
2019-09-06 15:49:01 +0900

    3

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -7,11 +7,11 @@ class PictureUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  if Rails.env.production?
-    storage :fog
-  else
-    storage :file
-  end
+  # if Rails.env.production?
+  #   storage :fog
+  # else
+  #   storage :file
+  # end
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
